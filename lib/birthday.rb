4ceff626c497f8ed9c766c747899520d2b51aaf5
Christopher Carter <chrisjohncarter123@gmail.com>
2020-02-17 03:10:10 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,13 +4,18 @@
 #   "Amanda" => 27
 # }
 
+#"only prints the birthday greeting if the birthday kid 12 or younger"
+
 #"Happy Birthday Timmy! You are now 9 years old!\nHappy Birthday Sarah! You are now 6 years old!\nYou are too old for this.\n" 
 
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    if(age <)
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    if(age <= 12)
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    else
+      puts "You are too old for this."
+    end
   end
 end
 
