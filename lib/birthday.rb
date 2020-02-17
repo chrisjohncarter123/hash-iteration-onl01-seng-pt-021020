c646f1ac83ea50d2216ae0df50568b34c017b9ce
Christopher Carter <chrisjohncarter123@gmail.com>
2020-02-17 03:10:46 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -14,4 +14,11 @@ def happy_birthday(birthday_kids)
   end
 end
 
+def happy_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    if(age <)
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+end
+
 
